2e85da4930118d27c2fe83e81deae00016f4757a
NicholasPilotto <nicholaspilotto@gmail.com>
2023-08-24 16:39:24 +0200

    Setting up project

--- RickAndMorty/RickAndMorty/SceneDelegate.swift
+++ RickAndMorty/RickAndMorty/SceneDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+// swiftlint:disable all
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
   var window: UIWindow?
@@ -49,4 +50,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
 }
-
+// swiftlint:enable all
