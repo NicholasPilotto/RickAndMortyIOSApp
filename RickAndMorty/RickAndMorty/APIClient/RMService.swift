570cfc900fe14ec39243e4d174e8ff8a334d838b
NicholasPilotto <nicholaspilotto@gmail.com>
2023-08-25 11:51:02 +0200

    Api service class

--- RickAndMorty/RickAndMorty/APIClient/RMService.swift
+++ RickAndMorty/RickAndMorty/APIClient/RMService.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+/// Pirmary API service object to get Rick and Morty data
+final class RMService {
+  /// Singleton instance of the class
+  static let shared = RMService()
+  
+  private init() {}
+  
+  
+  /// Send Rick and Morty API call
+  /// - Parameters:
+  ///   - request: request instance
+  ///   - completion: callback data or error
+  public func execute(_ request: RMRequest, completion: @escaping () -> Void) {
+  }
+}
