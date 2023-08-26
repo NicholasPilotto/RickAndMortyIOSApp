3d67ab2483914f6b518e0f107f39c40a2648c5cb
NicholasPilotto <nicholaspilotto@gmail.com>
2023-08-26 15:16:49 +0200

    Make request call

--- RickAndMorty/RickAndMorty/APIClient/RMService.swift
+++ RickAndMorty/RickAndMorty/APIClient/RMService.swift
@@ -12,7 +12,22 @@ final class RMService {
   /// Singleton instance of the class
   static let shared = RMService()
   
+  enum RMServiceError: Error {
+    case failedToCreateRequest
+    case failedToGetData
+  }
+  
   private init() {}
+  
+  private func request(from rmRequest: RMRequest) -> URLRequest? {
+    guard let url = rmRequest.url else {
+      return nil
+    }
+    
+    var request = URLRequest(url: url)
+    request.httpMethod = rmRequest.httpMethod
+    return request
+  }
   
   /// Send Rick and Morty API call
   /// - Parameters:
@@ -24,5 +39,25 @@ final class RMService {
     expecting type: T.Type,
     completion: @escaping (Result<T, Error>) -> Void
   ) {
+    guard let urlRequest = self.request(from: request) else {
+      completion(.failure(RMServiceError.failedToCreateRequest))
+      return
+    }
+    
+    let task = URLSession.shared.dataTask(with: urlRequest) { data, _, error in
+      guard let data = data, error == nil else {
+        completion(.failure(error ?? RMServiceError.failedToGetData))
+        return
+      }
+      
+      do {
+        let result = try JSONDecoder().decode(type.self, from: data)
+        completion(.success(result))
+      } catch {
+        completion(.failure(error))
+      }
+    }
+    
+    task.resume()
   }
 }
