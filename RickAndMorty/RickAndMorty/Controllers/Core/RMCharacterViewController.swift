b233fc8440ecb0eb38972b578a51bcb2806fd1b8
NicholasPilotto <nicholaspilotto@gmail.com>
2023-08-26 14:44:09 +0200

    Test request construction

--- RickAndMorty/RickAndMorty/Controllers/Core/RMCharacterViewController.swift
+++ RickAndMorty/RickAndMorty/Controllers/Core/RMCharacterViewController.swift
@@ -13,5 +13,24 @@ final class RMCharacterViewController: UIViewController {
     super.viewDidLoad()
     self.title = "Characters"
     view.backgroundColor = .systemBackground
+    
+    let request = RMRequest(
+      endpoint: .character,
+      pathComponents: ["1"],
+      queryParameters: [
+        URLQueryItem(name: "name", value: "rick"),
+        URLQueryItem(name: "status", value: "alive")
+      ])
+    
+    print(request.url ?? "error")
+    
+    RMService.shared.execute(request, expecting: RMCharacter.self) { result in
+      switch result {
+        case .success(let character):
+          print(character.id)
+        case .failure(let error):
+          print(error)
+      }
+    }
   }
 }
