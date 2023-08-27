e29766d0dc15ae52fb0eb8a5ab17e7d06d05aa2b
NicholasPilotto <nicholaspilotto@gmail.com>
2023-08-27 15:18:52 +0200

    Renamed class

---
+++ RickAndMorty/RickAndMorty/ViewModels/RMCharacterCollectionViewCellViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RMCharacterCollectionViewCellViewModel.swift
+//  RickAndMorty
+//
+//  Created by Nicholas Pilotto on 27/08/23.
+//
+
+import Foundation
---
+++ RickAndMorty/RickAndMorty/Views/RMCharacterCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  RMCharacterCollectionViewCell.swift
+//  RickAndMorty
+//
+//  Created by Nicholas Pilotto on 27/08/23.
+//
+
+import UIKit
+
+class RMCharacterCollectionViewCell: UICollectionViewCell {
+    
+}
