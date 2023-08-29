6fd87eff3a8b9b948f2a6b0df1c9a15e3b5bc4e5
NicholasPilotto <nicholaspilotto@gmail.com>
2023-08-29 21:08:28 +0200

    Single character detail view

---
+++ RickAndMorty/RickAndMorty/ViewModels/RMCharacterDetailViewViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RMCharacterDetailViewViewModel.swift
+//  RickAndMorty
+//
+//  Created by Nicholas Pilotto on 29/08/23.
+//
+
+import Foundation
---
+++ RickAndMorty/RickAndMorty/Views/RMCharacterDetailView.swift
@@ -0,0 +1,11 @@
+//
+//  RMCharacterDetailView.swift
+//  RickAndMorty
+//
+//  Created by Nicholas Pilotto on 29/08/23.
+//
+
+import UIKit
+
+final class RMCharacterDetailView: UIView {
+}
