171f8a6465f027bca9f6100c96276ba506a4b3e4
NicholasPilotto <nicholaspilotto@gmail.com>
2023-08-30 21:16:19 +0200

    Characters list footer view

---
+++ RickAndMorty/RickAndMorty/Views/RMFooterLoadingCollectionReusableView.swift
@@ -0,0 +1,43 @@
+//
+//  RMFooterLoadingCollectionReusableView.swift
+//  RickAndMorty
+//
+//  Created by Nicholas Pilotto on 30/08/23.
+//
+
+import UIKit
+
+final class RMFooterLoadingCollectionReusableView: UICollectionReusableView {
+  static let identifier = "RMFooterLoadingCollectionReusableView"
+  
+  private let spinner: UIActivityIndicatorView = {
+    let spinner = UIActivityIndicatorView()
+    spinner.hidesWhenStopped = true
+    spinner.translatesAutoresizingMaskIntoConstraints = false
+    return spinner
+  }()
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    backgroundColor = .systemBackground
+    addSubviews(spinner)
+    addContraints()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func addContraints() {
+    NSLayoutConstraint.activate([
+      spinner.widthAnchor.constraint(equalToConstant: 100),
+      spinner.heightAnchor.constraint(equalToConstant: 100),
+      spinner.centerXAnchor.constraint(equalTo: centerXAnchor),
+      spinner.centerYAnchor.constraint(equalTo: centerYAnchor)
+    ])
+  }
+  
+  public func startAnimating() {
+    spinner.startAnimating()
+  }
+}
