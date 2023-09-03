4758783782bea0954b88ad2ab0a2516eac12acab
NicholasPilotto <nicholaspilotto@gmail.com>
2023-09-03 17:54:54 +0200

    Episode detail view controller

--- RickAndMorty/RickAndMorty/Controllers/Other/RMEpisodeDetailViewController.swift
+++ RickAndMorty/RickAndMorty/Controllers/Other/RMEpisodeDetailViewController.swift
@@ -7,23 +7,22 @@
 
 import UIKit
 
-class RMEpisodeDetailViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+final class RMEpisodeDetailViewController: UIViewController {
+  private let url: URL?
+  
+  init(url: URL?) {
+    self.url = url
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemBackground
+    title = "Episode"
+  }
 }
