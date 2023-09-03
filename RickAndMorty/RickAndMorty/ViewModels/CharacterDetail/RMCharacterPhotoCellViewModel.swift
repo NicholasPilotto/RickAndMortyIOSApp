56d0dbe1fc2d1ad62fb4fb820fd73a7f3cc358e6
NicholasPilotto <nicholaspilotto@gmail.com>
2023-09-03 11:22:44 +0200

    Image view and fetch character image

--- RickAndMorty/RickAndMorty/ViewModels/CharacterDetail/RMCharacterPhotoCellViewModel.swift
+++ RickAndMorty/RickAndMorty/ViewModels/CharacterDetail/RMCharacterPhotoCellViewModel.swift
@@ -8,5 +8,20 @@
 import Foundation
 
 final class RMCharacterPhotoCellViewModel {
-  init() {}
+  private let imageUrl: URL?
+  
+  init(imageUrl: URL?) {
+    self.imageUrl = imageUrl
+  }
+  
+  /// Fetch character detail image
+  /// - Parameter completion: Completion handler block
+  public func fetchImage(completion: @escaping (Result<Data, Error>) -> Void) {
+    guard let imageUrl = imageUrl else {
+      completion(.failure(URLError(.badURL)))
+      return
+    }
+    
+    RMImageLoader.shared.downloadImage(imageUrl, completion: completion)
+  }
 }
