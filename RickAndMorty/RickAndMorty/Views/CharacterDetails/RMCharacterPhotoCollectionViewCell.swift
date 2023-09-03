03fa98885e8f938163994b7a28902dfeba1f3865
NicholasPilotto <nicholaspilotto@gmail.com>
2023-09-03 11:23:00 +0200

    Image view and fetch character image

--- RickAndMorty/RickAndMorty/Views/CharacterDetails/RMCharacterPhotoCollectionViewCell.swift
+++ RickAndMorty/RickAndMorty/Views/CharacterDetails/RMCharacterPhotoCollectionViewCell.swift
@@ -10,19 +10,48 @@ import UIKit
 final class RMCharacterPhotoCollectionViewCell: UICollectionViewCell {
   static let identifier = "RMCharacterPhotoCollectionViewCell"
   
+  private let imageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.contentMode = .scaleAspectFill
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    imageView.clipsToBounds = true
+    return imageView
+  }()
+  
   override init(frame: CGRect) {
     super.init(frame: frame)
+    contentView.addSubviews(imageView)
+    setUpConstraints()
   }
   
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
   
-  private func setUpConstraints() {}
+  private func setUpConstraints() {
+    NSLayoutConstraint.activate([
+      imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+      imageView.leftAnchor.constraint(equalTo: contentView.leftAnchor),
+      imageView.rightAnchor.constraint(equalTo: contentView.rightAnchor),
+      imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+    ])
+  }
   
   override func prepareForReuse() {
     super.prepareForReuse()
+    self.imageView.image = nil
   }
   
-  public func configure(with viewModel: RMCharacterPhotoCellViewModel) { }
+  public func configure(with viewModel: RMCharacterPhotoCellViewModel) {
+    viewModel.fetchImage { [weak self] result in
+      switch result {
+        case .success(let data):
+          DispatchQueue.main.async {
+            self?.imageView.image = UIImage(data: data)
+          }
+        case .failure(let error):
+          print(error.localizedDescription)
+      }
+    }
+  }
 }
