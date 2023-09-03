2c8ac14e2570e4e6258f6823357b955464826302
NicholasPilotto <nicholaspilotto@gmail.com>
2023-09-03 17:04:01 +0200

    Fetch episode and call publisher-subscriber block method

--- RickAndMorty/RickAndMorty/ViewModels/CharacterDetail/RMCharacterEpisodeCellViewModel.swift
+++ RickAndMorty/RickAndMorty/ViewModels/CharacterDetail/RMCharacterEpisodeCellViewModel.swift
@@ -7,10 +7,61 @@
 
 import Foundation
 
+protocol RMEpisodeDataRender {
+  var episode: String { get }
+  var air_date: String { get }
+  var name: String { get }
+}
+
 final class RMCharacterEpisodeCellViewModel {
   private let episodeDataUrl: URL?
+  private var isFetching = false
+  private var dataBlock: ((RMEpisodeDataRender) -> Void)?
+  
+  private var episode: RMEpisode? {
+    didSet {
+      guard let model = episode else {
+        return
+      }
+      
+      self.dataBlock?(model)
+    }
+  }
   
   init(episodeDataUrl: URL?) {
     self.episodeDataUrl = episodeDataUrl
+  }
+  
+  /// Publisher-Subscriber block called when episode is fetched
+  /// - Parameter block: Block execute when episode is fetched
+  public func registerForData(_ block: @escaping (RMEpisodeDataRender) -> Void) {
+    self.dataBlock = block
+  }
+  
+  /// Fetch episode data
+  public func fetchEpisode() {
+    guard !isFetching else {
+      if let model = episode {
+        self.dataBlock?(model)
+      }
+      return
+    }
+    
+    guard let url = episodeDataUrl, let rmRequest = RMRequest(url: url) else {
+      return
+    }
+    
+    isFetching = true
+    
+    RMService.shared.execute(rmRequest, expecting: RMEpisode.self) { [weak self] result in
+      switch result {
+        case .success(let model):
+          DispatchQueue.main.async {
+            self?.episode = model
+          }
+        case .failure(let failure):
+          print(failure)
+      }
+    }
   }
 }
