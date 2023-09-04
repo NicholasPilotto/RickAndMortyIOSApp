88ca8202ccfb3bb8060479bbfa170f36dfc8cfeb
NicholasPilotto <nicholaspilotto@gmail.com>
2023-09-04 21:35:26 +0200

    Fetch episode data

--- RickAndMorty/RickAndMorty/ViewModels/RMEpisodeDetailViewViewModel.swift
+++ RickAndMorty/RickAndMorty/ViewModels/RMEpisodeDetailViewViewModel.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+final class RMEpisodeDetailViewViewModel {
+  private let endpointUrl: URL?
+  
+  init(endpointUrl: URL?) {
+    self.endpointUrl = endpointUrl
+    
+    fetchEpisodeData()
+  }
+  
+  private func fetchEpisodeData() {
+    guard let url = endpointUrl, let request = RMRequest(url: url) else {
+      return
+    }
+    
+    RMService.shared.execute(request, expecting: RMEpisode.self) { result in
+      switch result {
+        case .success(let success):
+          print(success)
+        case .failure(let failure):
+          print(failure)
+      }
+    }
+  }
+}
