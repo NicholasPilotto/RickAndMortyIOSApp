0b6446208c5203be6aab06bc87529f213602b4d1
NicholasPilotto <nicholaspilotto@gmail.com>
2023-09-07 22:37:44 +0200

    Conform to RMEpisodeDetailViewViewModelDelegate

--- RickAndMorty/RickAndMorty/Views/RMEpisodeDetailView.swift
+++ RickAndMorty/RickAndMorty/Views/RMEpisodeDetailView.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 final class RMEpisodeDetailView: UIView {
+  private var viewModel: RMEpisodeDetailViewViewModel?
+  
+  private var collectionView: UICollectionView?
+  
+  private let spinner: UIActivityIndicatorView = {
+    let spinner = UIActivityIndicatorView()
+    spinner.translatesAutoresizingMaskIntoConstraints = false
+    return spinner
+  }()
+  
   override init(frame: CGRect) {
     super.init(frame: frame)
     translatesAutoresizingMaskIntoConstraints = false
+    
+    self.collectionView = createCollectionView()
+    
+    addConstraints()
   }
   
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func addConstraints() { }
+  
+  private func createCollectionView() -> UICollectionView {
+    return UICollectionView(frame: .zero)
+  }
+  
+  public func configure(with viewModel: RMEpisodeDetailViewViewModel) {
+    self.viewModel = viewModel
   }
 }
