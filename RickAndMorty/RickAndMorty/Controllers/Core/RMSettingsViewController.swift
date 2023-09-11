3433e2272c9768169ee1485ff1907b1aa810c5af
NicholasPilotto <nicholaspilotto@gmail.com>
2023-09-11 21:11:10 +0200

    Show safari view

--- RickAndMorty/RickAndMorty/Controllers/Core/RMSettingsViewController.swift
+++ RickAndMorty/RickAndMorty/Controllers/Core/RMSettingsViewController.swift
@@ -5,24 +5,13 @@
 //  Created by Nicholas Pilotto on 25/08/23.
 //
 
+import SafariServices
 import SwiftUI
 import UIKit
 
 /// Controller to show variuous app options and settings
 final class RMSettingsViewController: UIViewController {
-  private let viewModel = RMSettingsViewViewModel(cellViewModels:
-    RMSettingsOption.allCases.compactMap {
-      return RMSettingCellViewViewModel(type: $0)
-    }
-  )
-  
-  private let settingsSwiftUIController = UIHostingController(rootView: RMSettingsView(
-    viewModel: RMSettingsViewViewModel(
-      cellViewModels: RMSettingsOption.allCases.compactMap {
-        return RMSettingCellViewViewModel(type: $0)
-      })
-    )
-  )
+  private var settingsSwiftUIController: UIHostingController<RMSettingsView>?
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -33,6 +22,16 @@ final class RMSettingsViewController: UIViewController {
   }
   
   private func addSwiftUIControllers() {
+    let settingsSwiftUIController = UIHostingController(rootView: RMSettingsView(
+      viewModel: RMSettingsViewViewModel(
+        cellViewModels: RMSettingsOption.allCases.compactMap {
+          return RMSettingCellViewViewModel(type: $0) { [weak self] option in
+            self?.handleTap(option: option)
+          }
+        })
+      )
+    )
+    
     addChild(settingsSwiftUIController)
     settingsSwiftUIController.didMove(toParent: self)
     
@@ -54,5 +53,18 @@ final class RMSettingsViewController: UIViewController {
         equalTo: self.view.safeAreaLayoutGuide.bottomAnchor
       )
     ])
+    
+    self.settingsSwiftUIController = settingsSwiftUIController
+  }
+  
+  private func handleTap(option: RMSettingsOption) {
+    guard Thread.current.isMainThread else {
+      return
+    }
+    
+    if let url = option.targetUrl {
+      let viewController = SFSafariViewController(url: url)
+      present(viewController, animated: true)
+    } else if option == .rateApp {}
   }
 }
