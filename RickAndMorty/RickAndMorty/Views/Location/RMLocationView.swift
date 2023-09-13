474aad903bfddf0614673cf3e701e15b50db120d
NicholasPilotto <nicholaspilotto@gmail.com>
2023-09-13 21:00:01 +0200

    Location view

--- RickAndMorty/RickAndMorty/Views/Location/RMLocationView.swift
+++ RickAndMorty/RickAndMorty/Views/Location/RMLocationView.swift
@@ -8,13 +8,62 @@
 import UIKit
 
 class RMLocationView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+  private var viewModel: RMLocationViewModel? {
+    didSet {
+      spinner.stopAnimating()
+      tableView.isHidden = false
+      tableView.reloadData()
+      
+      UIView.animate(withDuration: 0.3) {
+        self.tableView.alpha = 1
+      }
     }
-    */
-
+  }
+  
+  private let tableView: UITableView = {
+    let table = UITableView()
+    table.alpha = 0
+    table.isHidden = true
+    table.translatesAutoresizingMaskIntoConstraints = false
+    table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    return table
+  }()
+  
+  private let spinner: UIActivityIndicatorView = {
+    let spinner = UIActivityIndicatorView()
+    spinner.translatesAutoresizingMaskIntoConstraints = false
+    spinner.hidesWhenStopped = true
+    return spinner
+  }()
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    backgroundColor = .systemBackground
+    translatesAutoresizingMaskIntoConstraints = false
+    addSubviews(tableView, spinner)
+    
+    spinner.startAnimating()
+    
+    addConstraints()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func addConstraints() {
+    NSLayoutConstraint.activate([
+      spinner.heightAnchor.constraint(equalToConstant: 100),
+      spinner.widthAnchor.constraint(equalToConstant: 100),
+      spinner.centerXAnchor.constraint(equalTo: centerXAnchor),
+      spinner.centerYAnchor.constraint(equalTo: centerYAnchor),
+      
+      tableView.topAnchor.constraint(equalTo: topAnchor),
+      tableView.leftAnchor.constraint(equalTo: leftAnchor),
+      tableView.rightAnchor.constraint(equalTo: rightAnchor),
+      tableView.bottomAnchor.constraint(equalTo: bottomAnchor)
+    ])
+  }
+  
+  public func configure(with viewModel: RMLocationViewModel) { }
 }
