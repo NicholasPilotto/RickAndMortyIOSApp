d5dc195634c221c7b87d067541f96c70c45352c5
NicholasPilotto <nicholaspilotto@gmail.com>
2023-09-13 21:00:30 +0200

    Show location view

--- RickAndMorty/RickAndMorty/Controllers/Core/RMLocationViewController.swift
+++ RickAndMorty/RickAndMorty/Controllers/Core/RMLocationViewController.swift
@@ -9,10 +9,18 @@ import UIKit
 
 /// Controller to show and search for locations
 final class RMLocationViewController: UIViewController {
+  private let locationView = RMLocationView()
+  
+  private let viewModel = RMLocationViewModel()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     self.title = "Locations"
     view.backgroundColor = .systemBackground
+    
+    view.addSubviews(locationView)
+    
+    addContraints()
     
     addSearchButton()
   }
@@ -21,6 +29,15 @@ final class RMLocationViewController: UIViewController {
     navigationItem.rightBarButtonItem = UIBarButtonItem(
       barButtonSystemItem: .search, target: self, action: #selector(didTapSearch)
     )
+  }
+  
+  private func addContraints() {
+    NSLayoutConstraint.activate([
+      locationView.topAnchor.constraint(equalTo: view.topAnchor),
+      locationView.leftAnchor.constraint(equalTo: view.leftAnchor),
+      locationView.rightAnchor.constraint(equalTo: view.rightAnchor),
+      locationView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+    ])
   }
   
   @objc private func didTapSearch() { }
