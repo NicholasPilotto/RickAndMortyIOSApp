a5279c6a79c2a20168848a6deba852c05bfdad47
NicholasPilotto <nicholaspilotto@gmail.com>
2023-09-14 21:30:39 +0200

    Locations response

---
+++ RickAndMorty/RickAndMorty/Models/APIResponseTypes/RMGetLocationsResponse.swift
@@ -0,0 +1,20 @@
+//
+//  RMGetLocationsResponse.swift
+//  RickAndMorty
+//
+//  Created by Nicholas Pilotto on 14/09/23.
+//
+
+import Foundation
+
+struct RMGetAllLocationsResponse: Codable {
+  struct Info: Codable {
+    let count: Int
+    let pages: Int
+    let next: String?
+    let prev: String?
+  }
+  
+  let info: Info
+  let results: [RMLocation]
+}
---
+++ RickAndMorty/RickAndMorty/ViewModels/Location/RMLocationTableViewCellViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RMLocationTableViewCellViewModel.swift
+//  RickAndMorty
+//
+//  Created by Nicholas Pilotto on 14/09/23.
+//
+
+import Foundation
