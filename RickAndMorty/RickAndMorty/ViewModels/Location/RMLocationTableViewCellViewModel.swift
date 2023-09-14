4ff13d8f4cc696d4bc29c71be95aabdbcc12f14f
NicholasPilotto <nicholaspilotto@gmail.com>
2023-09-14 21:31:04 +0200

    Start working on RMLocationTableViewCellViewModel

--- RickAndMorty/RickAndMorty/ViewModels/Location/RMLocationTableViewCellViewModel.swift
+++ RickAndMorty/RickAndMorty/ViewModels/Location/RMLocationTableViewCellViewModel.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+struct RMLocationTableViewCellViewModel: Hashable, Equatable {
+  private let location: RMLocation
+  
+  /// Location name
+  public var name: String {
+    return location.name
+  }
+  
+  /// Location type
+  public var type: String {
+    location.type
+  }
+  
+  /// Location dimension
+  public var dimension: String {
+    location.dimension
+  }
+  
+  init(location: RMLocation) {
+    self.location = location
+  }
+  
+  func hash(into hasher: inout Hasher) {
+    hasher.combine(name)
+    hasher.combine(location.id)
+    hasher.combine(dimension)
+    hasher.combine(location.type)
+  }
+  
+  static func == (lhs: RMLocationTableViewCellViewModel, rhs: RMLocationTableViewCellViewModel) -> Bool {
+    return lhs.hashValue == rhs.hashValue
+  }
+}
