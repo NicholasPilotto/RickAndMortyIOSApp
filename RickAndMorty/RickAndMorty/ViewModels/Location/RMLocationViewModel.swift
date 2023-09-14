7f6d329243510a07d57d54a5e1994f5b49e8e7a4
NicholasPilotto <nicholaspilotto@gmail.com>
2023-09-14 21:31:29 +0200

    Fetch data and create cell view models

--- RickAndMorty/RickAndMorty/ViewModels/Location/RMLocationViewModel.swift
+++ RickAndMorty/RickAndMorty/ViewModels/Location/RMLocationViewModel.swift
@@ -7,24 +7,51 @@
 
 import Foundation
 
+protocol RMLocationViewModelDelegate: AnyObject {
+  func didFetchInitialLocations()
+}
+
 final class RMLocationViewModel {
-  private var locations: [RMLocation] = []
-  
-  private var cellViewModels: [String] = []
+  private var locations: [RMLocation] = [] {
+    didSet {
+      for location in locations {
+        let cellViewModel = RMLocationTableViewCellViewModel(location: location)
+        
+        if !cellViewModels.contains(cellViewModel) {
+          cellViewModels.append(cellViewModel)
+        }
+      }
+    }
+}
   
   private var hasMoreResults: Bool {
     return false
   }
   
+  private var apiInfo: RMGetAllLocationsResponse.Info?
+  
+  /// Array of cell view models
+  public private(set) var cellViewModels: [RMLocationTableViewCellViewModel] = []
+  
+  /// RMLocationViewModelDelegate weak reference
+  public weak var delegate: RMLocationViewModelDelegate?
+  
   init() { }
   
+  /// Fetch locations function
   public func fetchLocation() {
-    RMService.shared.execute(.listLocationsRequests, expecting: String.self) { result in
+    RMService.shared.execute(
+      .listLocationsRequests,
+      expecting: RMGetAllLocationsResponse.self) { [weak self] result in
       switch result {
-        case .success(let success):
-          break
+        case .success(let model):
+          self?.apiInfo = model.info
+          self?.locations = model.results
+          DispatchQueue.main.async {
+            self?.delegate?.didFetchInitialLocations()
+          }
         case .failure(let failure):
-          break
+          print(failure)
       }
     }
   }
