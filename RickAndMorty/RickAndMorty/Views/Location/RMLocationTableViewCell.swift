ee73b300b13aca47764207cdef8ab888f4e4c3ed
NicholasPilotto <nicholaspilotto@gmail.com>
2023-09-14 21:33:25 +0200

    Start working on RMLocationTableViewCell

--- RickAndMorty/RickAndMorty/Views/Location/RMLocationTableViewCell.swift
+++ RickAndMorty/RickAndMorty/Views/Location/RMLocationTableViewCell.swift
@@ -8,16 +8,20 @@
 import UIKit
 
 class RMLocationTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+  static let cellIdentifier = "RMLocationTableViewCell"
+  
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    contentView.backgroundColor = .systemBackground
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func prepareForReuse() {
+    super.prepareForReuse()
+  }
+  
+  public func configure(with: RMLocationTableViewCellViewModel) { }
 }
