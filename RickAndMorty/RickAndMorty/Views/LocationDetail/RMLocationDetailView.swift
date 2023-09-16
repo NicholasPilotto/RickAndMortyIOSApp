b849122a827f7c7a6d1b5131b5f7d9dcf730f7f9
NicholasPilotto <nicholaspilotto@gmail.com>
2023-09-16 14:45:57 +0200

    Location detail view

--- RickAndMorty/RickAndMorty/Views/LocationDetail/RMLocationDetailView.swift
+++ RickAndMorty/RickAndMorty/Views/LocationDetail/RMLocationDetailView.swift
@@ -7,14 +7,234 @@
 
 import UIKit
 
-class RMLocationDetailView: UIView {
+protocol RMLocationDetailViewDelegate: AnyObject {
+  func rmEpisodeDetailView(_ detailView: RMLocationDetailView, didSelect character: RMCharacter)
+}
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+final class RMLocationDetailView: UIView {
+  private var viewModel: RMLocationDetailViewViewModel? {
+    didSet {
+      spinner.stopAnimating()
+      
+      self.collectionView?.isHidden = false
+      self.collectionView?.reloadData()
+      
+      UIView.animate(withDuration: 0.3) {
+        self.collectionView?.alpha = 1
+      }
     }
-    */
+  }
+  
+  private var collectionView: UICollectionView?
+  
+  private let spinner: UIActivityIndicatorView = {
+    let spinner = UIActivityIndicatorView()
+    spinner.translatesAutoresizingMaskIntoConstraints = false
+    spinner.hidesWhenStopped = true
+    return spinner
+  }()
+  
+  public weak var delegate: RMLocationDetailViewDelegate?
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    translatesAutoresizingMaskIntoConstraints = false
+    backgroundColor = .systemBackground
+    
+    let collectionView = createCollectionView()
+    self.collectionView = collectionView
+    
+    addSubviews(collectionView, spinner)
+    
+    addConstraints()
+    spinner.startAnimating()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func addConstraints() {
+    guard let collectionView = self.collectionView else {
+      return
+    }
+    
+    NSLayoutConstraint.activate([
+      spinner.heightAnchor.constraint(equalToConstant: 100),
+      spinner.widthAnchor.constraint(equalToConstant: 100),
+      spinner.centerXAnchor.constraint(equalTo: centerXAnchor),
+      spinner.centerYAnchor.constraint(equalTo: centerYAnchor),
+      
+      collectionView.topAnchor.constraint(equalTo: topAnchor),
+      collectionView.leftAnchor.constraint(equalTo: leftAnchor),
+      collectionView.rightAnchor.constraint(equalTo: rightAnchor),
+      collectionView.bottomAnchor.constraint(equalTo: bottomAnchor)
+    ])
+  }
+  
+  private func createCollectionView() -> UICollectionView {
+    let layout = UICollectionViewCompositionalLayout { [weak self] section, _ in
+      return self?.layout(for: section)
+    }
+    
+    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+    collectionView.translatesAutoresizingMaskIntoConstraints = false
+    collectionView.isHidden = true
+    collectionView.alpha = 0
+    collectionView.delegate = self
+    collectionView.dataSource = self
+    
+    collectionView.register(
+      RMEpisodeInfoCollectionViewCell.self,
+      forCellWithReuseIdentifier: RMEpisodeInfoCollectionViewCell.cellIdentifier
+    )
+    
+    collectionView.register(
+      RMCharacterCollectionViewCell.self,
+      forCellWithReuseIdentifier: RMCharacterCollectionViewCell.cellIdentifier
+    )
+    
+    return collectionView
+  }
+  
+  public func configure(with viewModel: RMLocationDetailViewViewModel) {
+    self.viewModel = viewModel
+  }
+}
 
+extension RMLocationDetailView {
+  private func layout(for section: Int) -> NSCollectionLayoutSection {
+    guard let sections = viewModel?.cellViewModels else {
+      return createInfoLayout()
+    }
+    
+    switch sections[section] {
+      case .information:
+        return createInfoLayout()
+      case .characters:
+        return createCharacterLayout()
+    }
+  }
+  
+  private func createInfoLayout() -> NSCollectionLayoutSection {
+    let item = NSCollectionLayoutItem(layoutSize: .init(
+      widthDimension: .fractionalWidth(1),
+      heightDimension: .fractionalHeight(1))
+    )
+    
+    item.contentInsets = NSDirectionalEdgeInsets(
+      top: 10,
+      leading: 10,
+      bottom: 10,
+      trailing: 10)
+    
+    let group = NSCollectionLayoutGroup.vertical(
+      layoutSize: .init(
+        widthDimension: .fractionalWidth(1),
+        heightDimension: .absolute(80)
+      ),
+      subitems: [item]
+    )
+    
+    let section = NSCollectionLayoutSection(group: group)
+    return section
+  }
+  
+  private func createCharacterLayout() -> NSCollectionLayoutSection {
+    let item = NSCollectionLayoutItem(layoutSize: .init(
+      widthDimension: .fractionalWidth(0.5),
+      heightDimension: .fractionalHeight(1))
+    )
+    
+    item.contentInsets = NSDirectionalEdgeInsets(
+      top: 5,
+      leading: 10,
+      bottom: 5,
+      trailing: 10)
+    
+    let group = NSCollectionLayoutGroup.horizontal(
+      layoutSize: .init(
+        widthDimension: .fractionalWidth(1),
+        heightDimension: .absolute(240)
+      ),
+      subitems: [item, item]
+    )
+    
+    let section = NSCollectionLayoutSection(group: group)
+    return section
+  }
+}
+
+extension RMLocationDetailView: UICollectionViewDelegate, UICollectionViewDataSource {
+  func numberOfSections(in collectionView: UICollectionView) -> Int {
+    return viewModel?.cellViewModels.count ?? 0
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    guard let sections = viewModel?.cellViewModels else {
+      return 0
+    }
+    
+    let sectionType = sections[section]
+    
+    switch sectionType {
+      case .information(let viewModels):
+        return viewModels.count
+      case .characters(let viewModels):
+        return viewModels.count
+    }
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    guard let sections = viewModel?.cellViewModels else {
+      fatalError("No view model")
+    }
+    
+    let sectionType = sections[indexPath.section]
+    
+    switch sectionType {
+      case .information(let viewModels):
+        let cellViewModel = viewModels[indexPath.row]
+        guard let cell = collectionView.dequeueReusableCell(
+          withReuseIdentifier: RMEpisodeInfoCollectionViewCell.cellIdentifier,
+          for: indexPath
+        ) as? RMEpisodeInfoCollectionViewCell else {
+          fatalError("Cannot deque RMEpisodeInfoCollectionViewCell")
+        }
+        cell.configure(with: cellViewModel)
+        return cell
+      case .characters(let viewModels):
+        let cellViewModel = viewModels[indexPath.row]
+        guard let cell = collectionView.dequeueReusableCell(
+          withReuseIdentifier: RMCharacterCollectionViewCell.cellIdentifier,
+          for: indexPath
+        ) as? RMCharacterCollectionViewCell else {
+          fatalError("Cannot deque RMCharacterCollectionViewCell")
+        }
+        cell.configure(with: cellViewModel)
+        return cell
+    }
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    collectionView.deselectItem(at: indexPath, animated: true)
+    
+    guard let viewModel = viewModel else {
+      return
+    }
+    
+    let sections = viewModel.cellViewModels
+    let sectionType = sections[indexPath.section]
+    
+    switch sectionType {
+      case .information:
+        break
+      case .characters:
+        guard let character = viewModel.character(at: indexPath.row) else {
+          return
+        }
+        
+        delegate?.rmEpisodeDetailView(self, didSelect: character)
+    }
+  }
 }
