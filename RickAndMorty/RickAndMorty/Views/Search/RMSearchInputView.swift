97bd968a908cecc932da1c1868d6ea6adc82f63f
NicholasPilotto <nicholaspilotto@gmail.com>
2023-09-17 21:37:26 +0200

    Start working on search functionality

--- RickAndMorty/RickAndMorty/Views/Search/RMSearchInputView.swift
+++ RickAndMorty/RickAndMorty/Views/Search/RMSearchInputView.swift
@@ -7,14 +7,5 @@
 
 import UIKit
 
-class RMSearchInputView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
+final class RMSearchInputView: UIView {
 }
--- RickAndMorty/RickAndMorty/Views/Search/RMSearchView.swift
+++ RickAndMorty/RickAndMorty/Views/Search/RMSearchView.swift
@@ -8,13 +8,35 @@
 import UIKit
 
 class RMSearchView: UIView {
+  private let viewModel: RMSearchViewViewModel
+  
+  init(frame: CGRect, viewModel: RMSearchViewViewModel) {
+    self.viewModel = viewModel
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+    super.init(frame: frame)
+    backgroundColor = .systemBackground
+    translatesAutoresizingMaskIntoConstraints = false
+    
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
 
+// MARK: CollectionView delegate and datasource methods
+
+extension RMSearchView: UICollectionViewDelegate, UICollectionViewDataSource {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 1
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+    return cell
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    collectionView.deselectItem(at: indexPath, animated: true)
+  }
 }
