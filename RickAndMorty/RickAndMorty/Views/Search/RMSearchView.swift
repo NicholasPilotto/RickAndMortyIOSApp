216084fa7b618ba38a303d1696f19d47d4f8bdd6
NicholasPilotto <nicholaspilotto@gmail.com>
2023-09-19 22:03:47 +0200

    show no results view

--- RickAndMorty/RickAndMorty/Views/Search/RMSearchView.swift
+++ RickAndMorty/RickAndMorty/Views/Search/RMSearchView.swift
@@ -10,6 +10,8 @@ import UIKit
 class RMSearchView: UIView {
   private let viewModel: RMSearchViewViewModel
   
+  private let noResultsView = RMNoSearchResultsView()
+  
   init(frame: CGRect, viewModel: RMSearchViewViewModel) {
     self.viewModel = viewModel
 
@@ -17,10 +19,22 @@ class RMSearchView: UIView {
     backgroundColor = .systemBackground
     translatesAutoresizingMaskIntoConstraints = false
     
+    addSubviews(noResultsView)
+    
+    addConstraints()
   }
   
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func addConstraints() {
+    NSLayoutConstraint.activate([
+      noResultsView.widthAnchor.constraint(equalToConstant: 150),
+      noResultsView.heightAnchor.constraint(equalToConstant: 150),
+      noResultsView.centerXAnchor.constraint(equalTo: centerXAnchor),
+      noResultsView.centerYAnchor.constraint(equalTo: centerYAnchor)
+    ])
   }
 }
 
