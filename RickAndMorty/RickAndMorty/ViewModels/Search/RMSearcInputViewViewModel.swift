bc1565240e2d2bd4c99062602ff5154504316478
NicholasPilotto <nicholaspilotto@gmail.com>
2023-09-20 21:22:01 +0200

    Setting up search input view model

--- RickAndMorty/RickAndMorty/ViewModels/Search/RMSearcInputViewViewModel.swift
+++ RickAndMorty/RickAndMorty/ViewModels/Search/RMSearcInputViewViewModel.swift
@@ -8,5 +8,51 @@
 import Foundation
 
 final class RMSearchInputViewViewModel {
-  init() { }
+  private let type: RMSearchConfig.`Type`
+  
+  public enum DynamicOptions: String {
+    case status = "Status"
+    case gender = "Gender"
+    case locationType = "Location type"
+  }
+  
+  /// Property to know if the current type has dynamic
+  /// options.
+  public var hasDynamicOptions: Bool {
+    switch self.type {
+      case .character, .location:
+        return true
+      case .episode:
+        return false
+    }
+  }
+  
+  /// Property to know what option current view needs to show.
+  public var options: [DynamicOptions] {
+    switch self.type {
+      case .character:
+        return [.status, .gender]
+      case .location:
+        return [.locationType]
+      case .episode:
+        return []
+    }
+  }
+  
+  /// Property to know what is the placeholder text
+  /// for the search bar.
+  public var searchPlaceholderText: String {
+    switch self.type {
+      case .character:
+        return "Character name"
+      case .location:
+        return "Location name"
+      case .episode:
+        return "Episode name"
+    }
+  }
+  
+  init(type: RMSearchConfig.`Type`) {
+    self.type = type
+  }
 }
