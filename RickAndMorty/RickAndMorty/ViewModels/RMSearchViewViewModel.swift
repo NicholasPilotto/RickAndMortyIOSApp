078f2ef355ab7b3bec287f5c074b9a89a50b67dc
NicholasPilotto <nicholaspilotto@gmail.com>
2023-09-24 21:22:02 +0200

    Change block, option map and query text

--- RickAndMorty/RickAndMorty/ViewModels/RMSearchViewViewModel.swift
+++ RickAndMorty/RickAndMorty/ViewModels/RMSearchViewViewModel.swift
@@ -8,10 +8,44 @@
 import Foundation
 
 final class RMSearchViewViewModel {
+  private var optionMap: [RMSearchInputViewViewModel.DynamicOptions: String] = [:]
+  private var searchText: String = String()
+  
+  private var registerOptionChangeBlock: (((RMSearchInputViewViewModel.DynamicOptions, String)) -> Void)?
+  
   /// Search configuration property
   public let config: RMSearchConfig
   
   init(config: RMSearchConfig) {
     self.config = config
   }
+  
+  // MARK: - Public methods
+  
+  /// Set value in option map
+  /// - Parameters:
+  ///   - value: value to store into the map
+  ///   - option: key pointing to the value
+  public func set(value: String, for option: RMSearchInputViewViewModel.DynamicOptions) {
+    optionMap[option] = value
+    let tuple = (option, value)
+    self.registerOptionChangeBlock?(tuple)
+  }
+  
+  /// Block executed when option is changed
+  /// - Parameter block: Callback
+  public func registerOptionChangeBlock(
+    _ block: @escaping ((RMSearchInputViewViewModel.DynamicOptions, String)) -> Void
+  ) {
+    self.registerOptionChangeBlock = block
+  }
+  
+  /// Set query text
+  /// - Parameter text: Search query parameters
+  public func set(query text: String) {
+    self.searchText = text
+  }
+  
+  /// Search something
+  public func executeSearch() { }
 }
